51bdc4a417c1ab70ef56c189859173d66727fe21
BKinya <gitongakinya1@gmail.com>
2019-01-18 21:30:36 +0300

    complete

--- src/EvenFibonacciNumbers.java
+++ src/EvenFibonacciNumbers.java
@@ -3,59 +3,34 @@ import java.util.ArrayList;
 public class EvenFibonacciNumbers {
 
     static int num = 1, num1 = 2, num2;
-    static ArrayList<Integer> numFiboSeries = new ArrayList<>();
+
 
     public static void main(String[] args) {
 
-        /**
-         * add the first two digits of the fibonacci series to the arrayList
-         */
-        numFiboSeries.add(num);
-        numFiboSeries.add(num1);
-
-        //an arrayList of values of fibonacci seriies less than [100]
-        ArrayList<Integer> fiboSeriesNumbers = fibonacciSeries(100);
-        //sum of even numbers in the arrayList
-        int finalSum = addEvenNumbers(fiboSeriesNumbers);
-
-        System.out.println(finalSum);
-
+       System.out.println(addEvenNumbers(100));
     }
+
 
     /**
      *
-     * @param max ; the maximum value wich the fibonacci sequence values should not exceed
-     * @return an arrayList of the values in the fibonacci series
-     */
-
-    public static ArrayList<Integer> fibonacciSeries(int max) {
-        num2 = num + num1;
-
-        if (num2 < max) {
-            num = num1;
-            num1 = num2;
-            numFiboSeries.add(num2);
-
-           return fibonacciSeries(max);
-        }
-        return numFiboSeries;
-
-    }
-
-    /**
-     *
-     * @param numbers ; an arraylist of numbers
+     * @param max ; the maximum limit
      * @return sum of even numbers in the arrayList
      */
-    public static int addEvenNumbers(ArrayList<Integer> numbers) {
-        int sum = 0;
+    public static int addEvenNumbers( int max) {
 
-        for (int i = 0; i < numbers.size(); i++) {
-            if (numbers.get(i) % 2 == 0) {
-                sum += numbers.get(i);
+        int previous = 1, current = 2, next = 0,   sum = 0;
 
+        //check if the current fib number has reached maximum value
+        while (current < max ){
+            if (current % 2 == 0 ){
+                sum += current;
             }
+            next = previous + current;
+            previous = current;
+            current = next;
         }
+
+
         return sum;
 
     }
